9fb18448598d9fca3b7a046b44d2bc2b28ea91cb
Mirko Maggioni <mirko.maggioni@gmail.com>
2018-09-01 18:37:51 +0200

    add multiclass classification tests

---
+++ MachineLearningLibrary/Models/GlassData.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.ML.Runtime.Api;
+
+namespace MachineLearningLibrary.Models
+{
+	public class GlassData
+	{
+		[Column("0")]
+		public float IdNumber;
+		[Column("1")]
+		public float RefractiveIndex;
+		[Column("2")]
+		public float Sodium;
+		[Column("3")]
+		public float Magnesium;
+		[Column("4")]
+		public float Aluminium;
+		[Column("5")]
+		public float Silicon;
+		[Column("6")]
+		public float Potassium;
+		[Column("7")]
+		public float Calcium;
+		[Column("8")]
+		public float Barium;
+		[Column("9")]
+		public float Iron;
+		[Column("10", "Label")]
+		public string Type;
+	}
+
+	public class GlassTypePrediction : MultiClassificationPrediction { }
+}
---
+++ MachineLearningLibraryTests/GlassDataMulticlassClassificationTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using MachineLearningLibrary.Models;
+using MachineLearningLibrary.Services;
+using Microsoft.ML.Models;
+using Microsoft.ML.Trainers;
+using NUnit.Framework;
+
+namespace MachineLearningLibraryTests
+{
+	[TestFixture]
+	public class GlassDataMulticlassClassificationTests
+	{
+		private PredictionService predictionService = new PredictionService();
+		private PipelineParameters<GlassData> _pipelineParameters;
+		private PipelineParameters<GlassData> _pipelineTestParameters;
+
+		[SetUp]
+		public void Setup()
+		{
+			var dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+			var dataPath = $@"{dir}\traindata\glass.csv";
+			var testDataPath = $@"{dir}\testdata\glass.csv";
+			var separator = ',';
+			var dictionarizedLabels = new[] { "Label" };
+			var concatenatedColumns = new[] { "IdNumber", "RefractiveIndex", "Sodium", "Magnesium", "Aluminium", "Silicon", "Potassium", "Calcium", "Barium", "Iron" };
+			_pipelineParameters = new PipelineParameters<GlassData>(dataPath, separator, "PredictedLabel", null, dictionarizedLabels, concatenatedColumns);
+			_pipelineTestParameters = new PipelineParameters<GlassData>(testDataPath, separator);
+		}
+
+		[Test]
+		public async Task GlassDataMulticlassClassificationTest()
+		{
+			var modelPath = await predictionService.TrainAsync<GlassData, GlassTypePrediction, NaiveBayesClassifier>(_pipelineParameters);
+			var result = await predictionService.EvaluateClassificationAsync<GlassData, GlassTypePrediction>(_pipelineTestParameters, modelPath);
+			LogResult(nameof(NaiveBayesClassifier), result);
+
+			modelPath = await predictionService.TrainAsync<GlassData, GlassTypePrediction, LogisticRegressionClassifier>(_pipelineParameters);
+			result = await predictionService.EvaluateClassificationAsync<GlassData, GlassTypePrediction>(_pipelineTestParameters, modelPath);
+			LogResult(nameof(LogisticRegressionClassifier), result);
+
+			modelPath = await predictionService.TrainAsync<GlassData, GlassTypePrediction, StochasticDualCoordinateAscentClassifier>(_pipelineParameters);
+			result = await predictionService.EvaluateClassificationAsync<GlassData, GlassTypePrediction>(_pipelineTestParameters, modelPath);
+			LogResult(nameof(StochasticDualCoordinateAscentClassifier), result);
+		}
+
+		private void LogResult(string algorithm, ClassificationMetrics classificationMetrics)
+		{
+			Console.WriteLine($"------------- {algorithm} - EVALUATION RESULTS -------------");
+			Console.WriteLine($"AccuracyMacro = {classificationMetrics.AccuracyMacro}");
+			Console.WriteLine($"AccuracyMicro = {classificationMetrics.AccuracyMicro}");
+			Console.WriteLine($"LogLoss = {classificationMetrics.LogLoss}");
+			Console.WriteLine($"LogLossReduction = {classificationMetrics.LogLossReduction}");
+			Console.WriteLine($"------------- {algorithm} - END EVALUATION -------------");
+		}
+	}
+}
