1d49d0a1ba0f8694d8b639076d5f7c02e0193c54
Mirko Maggioni <mirko.maggioni@gmail.com>
2019-04-18 14:21:12 +0200

    WIP updating to ML.NET v0.11

--- MachineLearningLibrary/Models/PipelineParameters.cs
+++ MachineLearningLibrary/Models/PipelineParameters.cs
@@ -1,52 +1,33 @@
-﻿using Microsoft.ML;
-using Microsoft.ML.Runtime.Data;
-using System;
+﻿using Microsoft.Data.DataView;
+using Microsoft.ML;
+using Microsoft.ML.Data;
 using System.Collections.Generic;
-using static Microsoft.ML.Runtime.Data.TextLoader;
+using System.Linq;
 
 namespace MachineLearningLibrary.Models
 {
 	public class PipelineParameters<T> where T : class
 	{
-		private readonly TextLoader _reader;
+		public readonly MLContext mlContext;
+		public readonly IDataView dataView;
+
+		private readonly string[] _dictionarizedLabels;
 		private string _predictedColumn;
-		private readonly string _dataPath;
 
-		public PipelineParameters(string dataPath, string separator, bool hasHeader, string predictedColumn = null, Tuple<string, DataKind, int, bool, bool>[] columns = null, TrainContextBase trainContextBase = null)
+		public PipelineParameters(string dataPath, char separator, string predictedColumn = null, string[] alphanumericColumns = null, string[] dictionarizedLabels = null, IEnumerable<string> concatenatedColumns = null)
 		{
-			var mlColumns = new List<Column>();
-			var mlFeatureColumns  = new List<Range>();
-			var mlTargetColumns  = new List<Range>();
+			mlContext = new MLContext();
+			dataView = mlContext.Data.LoadFromTextFile<T>(dataPath, separator, hasHeader: false);
+			mlContext.Transforms.Text.FeaturizeText(DefaultColumnNames.Features, concatenatedColumns, null);
 
-			foreach (var column in columns)
-			{
-				mlColumns.Add(new Column(column.Item1, column.Item2, column.Item3));
-				if (column.Item4)
-				{
-					mlFeatureColumns.Add(new Range(column.Item3));
-				}
-				if (column.Item5)
-				{
-					mlTargetColumns.Add(new Range(column.Item3));
-				}
-			}
-
-			mlColumns.Add(new Column("FeatureVector", DataKind.R4, mlFeatureColumns.ToArray()));
-			mlColumns.Add(new Column("Target", DataKind.R4, mlTargetColumns.ToArray()));
-
-			_reader = new TextLoader(new LocalEnvironment(), new Arguments
-			{
-				Column = mlColumns.ToArray(),
-				HasHeader = hasHeader,
-				Separator = separator
-			});
+			foreach (var alphanumericColumn in alphanumericColumns)
+				mlContext.Transforms.Categorical.OneHotEncoding(alphanumericColumn);
 
 			_predictedColumn = predictedColumn;
-            TrainContextBase = trainContextBase;
-			_dataPath = dataPath;
+			_dictionarizedLabels = dictionarizedLabels;
 		}
 
-		public IDataView Data => _reader.Read(new MultiFileSource(_dataPath));
-		public TrainContextBase TrainContextBase { get; }
+		public PredictedLabelColumnOriginalValueConverter PredictedLabelColumnOriginalValueConverter => !string.IsNullOrEmpty(_predictedColumn) ? new PredictedLabelColumnOriginalValueConverter { PredictedLabelColumn = _predictedColumn } : null;
+		public Dictionarizer Dictionarizer => _dictionarizedLabels != null ? new Dictionarizer(_dictionarizedLabels) : null;
 	}
 }
