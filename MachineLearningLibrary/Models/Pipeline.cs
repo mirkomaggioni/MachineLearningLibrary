0c7923a1b15d82a338dd11f6c0851103fa6f18f6
Mirko Maggioni <mirko.maggioni@gmail.com>
2019-06-19 17:39:30 +0200

    interfaces are implemented

---
+++ MachineLearningLibrary/Models/Pipeline.cs
@@ -0,0 +1,83 @@
+﻿using MachineLearningLibrary.Interfaces;
+using MachineLearningLibrary.Services;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Transforms;
+
+namespace MachineLearningLibrary.Models
+{
+	public class Pipeline<T> : IPredictedColumnPipeline, IAlphanumericColumnsConversionPipeline, IConcatenateColumns, ITrain where T : class
+	{
+		public readonly MLContext MlContext;
+		public readonly IDataView DataView;
+
+		private ValueToKeyMappingEstimator valueToKeyMappingEstimator;
+		private ColumnCopyingEstimator columnCopyingEstimator;
+		private EstimatorChain<ColumnCopyingTransformer> estimatorChainCopy;
+		private EstimatorChain<OneHotEncodingTransformer> estimatorChainEncoding;
+		private EstimatorChain<ColumnConcatenatingTransformer> estimatorChainTransformer;
+
+		public Pipeline(string dataPath, char separator)
+		{
+			MlContext = new MLContext();
+			DataView = MlContext.Data.LoadFromTextFile<T>(dataPath, separator, hasHeader: false);
+		}
+
+		public void ConvertAlphanumericKeyColumn(string column)
+		{
+			valueToKeyMappingEstimator = MlContext.Transforms.Conversion.MapValueToKey(column);
+		}
+
+		public IAlphanumericColumnsConversionPipeline CopyColumn(string outputColumnName, string inputColumnName)
+		{
+			if (valueToKeyMappingEstimator == null)
+			{
+				columnCopyingEstimator = MlContext.Transforms.CopyColumns(outputColumnName, inputColumnName);
+			}
+			else
+			{
+				estimatorChainCopy = valueToKeyMappingEstimator.Append(MlContext.Transforms.CopyColumns(outputColumnName, inputColumnName));
+			}
+
+			return this;
+		}
+
+		public IConcatenateColumns ConvertAlphanumericColumns(string[] columns)
+		{
+			for (int i = 0; i < columns.Length; i++)
+			{
+				if (i == 0)
+				{
+					estimatorChainEncoding = estimatorChainCopy != null 
+						? estimatorChainCopy.Append(MlContext.Transforms.Categorical.OneHotEncoding(columns[i]));
+						: columnCopyingEstimator.Append(MlContext.Transforms.Categorical.OneHotEncoding(columns[i]));
+				}
+				else
+				{
+					estimatorChainEncoding = estimatorChainEncoding.Append(MlContext.Transforms.Categorical.OneHotEncoding(columns[i]));
+				}
+			}
+
+			return this;
+		}
+
+		public ITrain ConcatenateColumns(string[] columns)
+		{
+			if (estimatorChainEncoding != null)
+			{
+				estimatorChainTransformer = estimatorChainEncoding.Append(MlContext.Transforms.Concatenate("Features", columns));
+			}
+			else
+			{
+				estimatorChainTransformer = columnCopyingEstimator.Append(MlContext.Transforms.Concatenate("Features", columns));
+			}
+
+			return this;
+		}
+
+		public void Train(AlgorithmType algorithmType)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
