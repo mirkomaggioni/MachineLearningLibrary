6590fe31199a1893b16c60247422193bab4ba43b
Mirko Maggioni <mirko.maggioni@gmail.com>
2019-06-19 09:00:58 +0200

    pipeline interfaces are defined

---
+++ MachineLearningLibrary/Interfaces/IPipeline.cs
@@ -0,0 +1,25 @@
+﻿using MachineLearningLibrary.Services;
+
+namespace MachineLearningLibrary.Interfaces
+{
+	public interface IPredictedColumnPipeline
+	{
+		void ConvertAlphanumericKeyColumn(string column);
+		IAlphanumericColumnsConversionPipeline CopyColumn(string outputColumnName, string inputColumnName);
+	}
+
+	public interface IAlphanumericColumnsConversionPipeline
+	{
+		IConcatenateColumns ConvertAlphanumericColumns(string[] columns);
+	}
+
+	public interface IConcatenateColumns
+	{
+		ITrain ConcatenateColumns(string[] columns);
+	}
+
+	public interface ITrain
+	{
+		void Train(AlgorithmType algorithmType);
+	}
+}
