d7e692c96644ace1c7d2ea74208e92b0f502470a
Mirko Maggioni <mirko.maggioni@gmail.com>
2019-08-06 14:11:42 +0200

    using prediction method in car data regression test

--- MachineLearningLibraryTests/CarDataRegressionEvaluationTests.cs
+++ MachineLearningLibraryTests/CarDataRegressionEvaluationTests.cs
@@ -54,6 +54,19 @@ namespace MachineLearningLibraryTests
 			LogResult(nameof(AlgorithmType.PoissonRegressor), result);
 		}
 
+		[Test]
+		[TestCase(0f, 102f, "subaru", "gas", "std", "four", "sedan", "fwd", "front", 97.20f, 172.00f, 65.40f, 52.50f, 2145f, "ohcf", "four", 108f, "2bbl", 3.62f, 2.64f, 9.50f, 82f, 4800f, 32f, 37f, 7126f)]
+		[TestCase(0f, 161f, "peugot", "gas", "std", "four", "sedan", "rwd", "front", 107.90f, 186.70f, 68.40f, 56.70f, 3075f, "l", "four", 120f, "mpfi", 3.46f, 2.19f, 8.40f, 95f, 5000f, 19f, 24f, 15580f)]
+		public void GlassDataClusteringPredictTest(float symboling, float normalizedLosses, string make, string fuelType, string aspiration, string doors, string bodyStyle, string driveWheels, string engineLocation, float wheelBase, float length, float width, float height, float curbWeight, string engineType, string numOfCylinders, float engineSize, string fuelSystem, float bore, float stroke, float compressionRatio, float horsePower, float peakRpm, float cityMpg, float highwayMpg, float price)
+		{
+			var pipeline = new Pipeline<CarData>(_dataPath, _separator, AlgorithmType.StochasticDualCoordinateAscentRegressor, (_predictedColumn, false, null), _concatenatedColumns, _alphanumericColumns);
+			pipeline.BuildModel();
+
+			var prediction = pipeline.PredictScore<CarData, CarPricePrediction, float>(new CarData() { Symboling = symboling, NormalizedLosses = normalizedLosses, Make = make, FuelType = fuelType, Aspiration = aspiration, Doors = doors, BodyStyle = bodyStyle, DriveWheels = driveWheels, EngineLocation = engineLocation, WheelBase = wheelBase, Length = length, Width = width, Height = height, CurbWeight = curbWeight, EngineType = engineType, NumOfCylinders = numOfCylinders, EngineSize = engineSize, FuelSystem = fuelSystem, Bore = bore, Stroke = stroke, CompressionRatio = compressionRatio, HorsePower = horsePower, PeakRpm = peakRpm, CityMpg = cityMpg, HighwayMpg = highwayMpg });
+
+			Assert.AreEqual(prediction.Score, price);
+		}
+
 		private void LogResult(string algorithm, RegressionMetrics regressionMetrics)
 		{
 			Console.WriteLine($"------------- {algorithm} - EVALUATION RESULTS -------------");
