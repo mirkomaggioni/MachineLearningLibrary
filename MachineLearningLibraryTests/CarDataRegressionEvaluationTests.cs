cbaafd35df70a2989b4e7e3ee777c137b895e76b
Mirko Maggioni <mirko.maggioni@gmail.com>
2019-08-26 11:49:48 +0200

    glass data clustering algorithm is changed

--- MachineLearningLibraryTests/CarDataRegressionEvaluationTests.cs
+++ MachineLearningLibraryTests/CarDataRegressionEvaluationTests.cs
@@ -58,14 +58,16 @@ namespace MachineLearningLibraryTests
 		[Test]
 		[TestCase(0f, 102f, "subaru", "gas", "std", "four", "sedan", "fwd", "front", 97.20f, 172.00f, 65.40f, 52.50f, 2145f, "ohcf", "four", 108f, "2bbl", 3.62f, 2.64f, 9.50f, 82f, 4800f, 32f, 37f, 7126f)]
 		[TestCase(0f, 161f, "peugot", "gas", "std", "four", "sedan", "rwd", "front", 107.90f, 186.70f, 68.40f, 56.70f, 3075f, "l", "four", 120f, "mpfi", 3.46f, 2.19f, 8.40f, 95f, 5000f, 19f, 24f, 15580f)]
+		[TestCase(0f, 91f, "toyota", "gas", "std", "four", "sedan", "fwd", "front", 95.70f, 166.30f, 64.40f, 53.00f, 2081f, "ohc", "four", 98f, "2bbl", 3.19f, 3.03f, 9.00f, 70f, 4800f, 30f, 37f, 6938f)]
 		public void GlassDataClusteringPredictTest(float symboling, float normalizedLosses, string make, string fuelType, string aspiration, string doors, string bodyStyle, string driveWheels, string engineLocation, float wheelBase, float length, float width, float height, float curbWeight, string engineType, string numOfCylinders, float engineSize, string fuelSystem, float bore, float stroke, float compressionRatio, float horsePower, float peakRpm, float cityMpg, float highwayMpg, float price)
 		{
-			var pipeline = new Pipeline<Car>(_dataPath, _separator, AlgorithmType.StochasticDualCoordinateAscentRegressor, new PredictedColumn(_predictedColumn), _concatenatedColumns, _alphanumericColumns);
+			var pipeline = new Pipeline<Car>(_dataPath, _separator, AlgorithmType.PoissonRegressor, new PredictedColumn(_predictedColumn), _concatenatedColumns, _alphanumericColumns);
 			pipeline.BuildModel();
 
 			var prediction = pipeline.PredictScore<Car, CarPricePrediction, float>(new Car() { Symboling = symboling, NormalizedLosses = normalizedLosses, Make = make, FuelType = fuelType, Aspiration = aspiration, Doors = doors, BodyStyle = bodyStyle, DriveWheels = driveWheels, EngineLocation = engineLocation, WheelBase = wheelBase, Length = length, Width = width, Height = height, CurbWeight = curbWeight, EngineType = engineType, NumOfCylinders = numOfCylinders, EngineSize = engineSize, FuelSystem = fuelSystem, Bore = bore, Stroke = stroke, CompressionRatio = compressionRatio, HorsePower = horsePower, PeakRpm = peakRpm, CityMpg = cityMpg, HighwayMpg = highwayMpg });
 
-			Assert.AreEqual(prediction.Score, price);
+			var abs = Math.Abs(prediction.Score - price);
+			Assert.IsTrue(abs < (price / 100 * 7));
 		}
 
 		private void LogResult(string algorithm, RegressionMetrics regressionMetrics)
